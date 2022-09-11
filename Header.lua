22d4a4852d98fa0b4ba713780602cd176725a395
Reynaldeeee <113040532+Reynaldeeee@users.noreply.github.com>
2022-09-11 16:55:31 +0700

    Update Header.lua

--- Header.lua
+++ Header.lua
@@ -1,23 +1,23 @@
 bot_raw = {}
 
-bot_raw["BOTNAME1"] = {
+bot_raw["RhyanBabu3"] = {
 --=======SETUP=======--
 webhooks = {
-  ["LINK"]  = '',
+  ["LINK"]  = 'https://discord.com/api/webhooks/1018448670758281256/eBj8mNw8bYHDLbPYoSsgCv3q_8nUcHmG00ti3JbNj-AqOmGUMFVEZ623bXSVgOPxJCXy',
   ["EDIT"]  = false
 }
 -----------------------
 bot_config = {
-  ["World"] = {"FARM1"},
-  ["Door"] = 'FARM_ID',
-  ["Loop"] = true,
+  ["World"] = {"YQGLW"},
+  ["Door"] = 'ALDIEE2',
+  ["Loop"] = false,
   ["Save_Mode"] = 1,
   ["Save"] = {
               {
-                ["World"] = "World_Save",
-                ["Door"] = "Door_ID",       --Mode 1
-                ["Path_Seed"] = 20,
-                ["Path_Pack"] = 12
+                ["World"] = "KOPITUBRUK12",
+                ["Door"] = "MANNZL,       --Mode 1
+                ["Path_Seed"] = 6388,
+                ["Path_Pack"] = 1290
               },
               {
                 ["World_Pack"] = "World_Save_Pack",
@@ -32,16 +32,16 @@ bot_config = {
 }
 ------------------------
 bot_mode = {
-  ["Block"] = 5666,
+  ["Block"] = 4584,
   ["Type"] = 'fg',
-  ["Tile_Hit"] = 5,
+  ["Tile_Hit"] = 1,
   ["Ignore Gems"] = false, --Ignore Gems When Harvesting
   ["Coordinate_Break"] = {2,1}
 }
 ------------------------
 event = {
   ["Anti_Toxic_Waste"] = true,
-  ["Event"] = true,
+  ["Event"] = false,
   ["Item_Event"] = {},
   ["Limit"] = 100
 }
@@ -52,7 +52,7 @@ bot_environment = {
               ["List"] = {5040,5042,5044,5032,5034,5036,5038,5024,5026,5028,5030,7164,7162}
               },
   ["Buy"] = {
-            ["Mode"] = true,
+            ["Mode"] = false,
             ["Name"] = 'world_lock',
             ["Price"] = 2000,
             ["Item_List"] = {242},
@@ -63,9 +63,9 @@ bot_environment = {
                         ["Break"]   = true
                        }
             },
-  ["Jammer"] = true,
+  ["Jammer"] = false,
   ["Ban"] = {
-            ["Mode"] = true,
+            ["Mode"] = false,
             ["Ignore"] = {"Ignored_Name"}
             },
   ["Leave"] = {
